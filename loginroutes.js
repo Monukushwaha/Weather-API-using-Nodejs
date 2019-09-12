d9a17639fda7cde23de5191718448c8b06dc5d69
monu kushwaha <monu17@navgurukul.org>
2019-09-12 13:25:19 +0530

    remove node module

--- loginroutes.js
+++ loginroutes.js
@@ -1,8 +1,25 @@
 
 //**************************** */ we create handler for user registration:
 
+
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'navgurukul',
+  database : 'api'
+});
+connection.connect(function(err){
+if(!err) {
+    console.log("Database is connected ... nn");
+} else {
+    console.log("Error connecting database ... nn");
+}
+});
+
 exports.register = function(req,res){
-    console.log("req",req.body.first_name);
+    // console.log("req",req.body.first_name);
     var today = new Date();
     var users={
       "first_name":req.body.first_name,
