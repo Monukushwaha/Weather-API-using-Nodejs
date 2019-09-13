db9e5402fad7e11494c17272c1dc637f1bdcbd3d
monu kushwaha <monu17@navgurukul.org>
2019-09-13 15:33:16 +0530

    resolved node module bug

--- server.js
+++ server.js
@@ -1,20 +1,6 @@
 
 // ***********************we create mysql connection using specifying the database of choice:
 
-// var mysql      = require('mysql');
-// var connection = mysql.createConnection({
-//   host     : 'localhost',
-//   user     : 'root',
-//   password : 'navgurukul',
-//   database : 'api'
-// });
-// connection.connect(function(err){
-// if(!err) {
-//     console.log("Database is connected ... nn");
-// } else {
-//     console.log("Error connecting database ... nn");
-// }
-// });
 
 var express    = require("express");
 var login = require('./loginroutes.js');
